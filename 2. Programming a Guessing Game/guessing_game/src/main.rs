9d6d57837e4a0c2135dba1aa16531e6316c7dd53
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2024-05-16 00:25:57 +0530

    UPDATE guessing game

--- 2. Programming a Guessing Game/guessing_game/src/main.rs
+++ 2. Programming a Guessing Game/guessing_game/src/main.rs
@@ -1,3 +1,31 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello from guessing GAME")
+    println!("Hello from guessing Game...");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("=== Guess a new number ===");
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("failed to read");
+        println!("You guessed {}", guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("your guess value was less"),
+            Ordering::Greater => println!("your guess value was greater"),
+            Ordering::Equal => {
+                println!("You WON !!!");
+                break;
+            }
+        }
+    }
 }
